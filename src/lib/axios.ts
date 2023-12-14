f78e5199deda9c69ac4fbd8e0b59de234cb105b1
Lucas Briz <73654573+lucasbriz@users.noreply.github.com>
2023-12-14 19:43:11 -0300

    Update axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://estante-geek-db.mysql.database.azure.com:3306/estante-geek-db',
+    baseURL: 'https://estante-geek.azurewebsites.net/',
 });
